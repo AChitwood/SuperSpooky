5d5e1e86d51d6a289ff08e3b51b350d3c5e81e3e
anfelder <anfelder@users.noreply.github.com>
2015-05-25 20:43:54 -0500

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,16 @@
+//this includes input/output stream library
 #include <iostream>
 
+//this allows the std namespace to be used without doing std:: for everything
 using namespace std;
 
+//this program prints hello to the console
 int main()
+//this bracket opens the main function
 {
+  //this part prints hello to the console
   cout << "Hello world." << endl;
+  //this part returns 0 
   return 0;
+//this part closes the main function
 }
